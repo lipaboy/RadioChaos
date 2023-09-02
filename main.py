125382544fb0d78fe33b16cf7634c1f394bfaf33
a.lipatkin <a.lipatkin@vega-absolute.ru>
2023-09-02 21:47:00 +0700

    Changes

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import asyncio
 import io
 import random
 
+import pyrogram.types
 from pyrogram import Client, filters
 
 import csv
 import json
 import time
 from pygame import mixer  # Load the popular external library
-from pyrogram.types import InputPhoneContact
+from pyrogram.types import InputPhoneContact, InputMediaAudio
 
 start = time.time()
 trackDB = []
@@ -31,30 +33,17 @@ with open("config.txt", encoding="utf-8") as configFile:
 app = Client("my_account", api_id=config["api_id"], api_hash=config["api_hash"])
 
 
-async def main():
-    async with app:
-        row = trackDB[random.randrange(len(trackDB))]
-        tracksStr = row[0] + " " + row[1]
-        print(tracksStr)
-        bot_results = await app.get_inline_bot_results(
-            "murglar_bot",
-            query=tracksStr
-        )
-
-        await app.send_inline_bot_result(
-            "me", bot_results.query_id,
-            bot_results.results[0].id)
+async def get_last_message():
+    async for message in app.get_chat_history("me", limit=1, offset_id=-1):
+        return message
 
 
-async def progress(current, total):
-    print(f"{current * 100 / total:.1f}%")
-
-
-@app.on_message(filters.me)
-async def my_handler(client, message):
+async def downloadTrackFromMessage(message):
     print("start downloading")
     file = await app.download_media(message, progress=progress, in_memory=True)
     print("finish downloading")
+
+    await app.delete_messages("me", message.id)
 
     # print(file)
 
@@ -66,8 +55,59 @@ async def my_handler(client, message):
         time.sleep(1)
 
 
-app.run()
-# app.run(main())
+async def getRandomTrack():
+    async with app:
+        row = trackDB[random.randrange(len(trackDB))]
+        tracksStr = row[0] + " " + row[1]
+        # print(tracksStr)
+        bot_results = await app.get_inline_bot_results(
+            "murglar_bot",
+            query=tracksStr
+        )
+
+        await app.send_inline_bot_result(
+            "me", bot_results.query_id,
+            bot_results.results[0].id)
+
+        # await asyncio.sleep(8)
+        trackMessage = await get_last_message()
+        while trackMessage.reply_markup is not None:
+            await asyncio.sleep(1)
+            trackMessage = await get_last_message()
+
+        print(trackMessage)
+        # await app.edit_message_media(
+        #     chat_id=trackMessage.chat.id,
+        #     message_id=trackMessage.id,
+        #     media=InputMediaAudio(
+        #         caption="lol.mp3",
+        #         media=str(trackMessage.audio.file_id)))
+        await app.send_audio(
+            chat_id="me",
+            audio=str(trackMessage.audio.file_id),
+            title="Title", performer="Performer",
+        )
+        trackMessage = await get_last_message()
+        print(trackMessage)
+        # await downloadTrackFromMessage(trackMessage)
+
+
+async def progress(current, total):
+    print(f"{current * 100 / total:.1f}%")
+
+
+@app.on_message(filters.me)
+async def my_handler(client, message):
+    await downloadTrackFromMessage(message)
+
+
+async def main():
+    async with app:
+        await app.run()
+
+
+app.run(getRandomTrack())
+# asyncio.run(main())
 
 
 # contact_phone = '+79538571643'
