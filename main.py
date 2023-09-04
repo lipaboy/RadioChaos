0fb552c63374d4c145c4189fffb1d1e4916604ab
a.lipatkin <a.lipatkin@vega-absolute.ru>
2023-09-04 16:21:41 +0700

    Сделал бесконечный поток треков@

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import random
 import pyrogram.types
 from pyrogram import Client, filters
 
-import csv
 import json
 import time
 from pygame import mixer  # Load the popular external library
 from pyrogram.types import InputPhoneContact, InputMediaAudio
 
-start = time.time()
+
 trackDB = []
 with open("tracksless.txt", encoding='utf-8') as tracksFile:
     while True:
@@ -23,8 +22,6 @@ with open("tracksless.txt", encoding='utf-8') as tracksFile:
             break
         trackRow = (groupName, songName)
         trackDB.append(trackRow)
-
-# print((time.time() - start) * 1000)
 
 
 with open("config.txt", encoding="utf-8") as configFile:
@@ -42,24 +39,27 @@ async def downloadTrackFromMessage(message):
     print("start downloading")
     file = await app.download_media(message, progress=progress, in_memory=True)
     print("finish downloading")
+    return file
 
-    await app.delete_messages("me", message.id)
 
-    # print(file)
-
+def playSongFromFile(file):
     mixer.init()
-    # mixer.music.load(file)
     mixer.music.load(io.BytesIO(file.getbuffer()))
     mixer.music.play()
     while mixer.music.get_busy():  # wait for music to finish playing
         time.sleep(1)
 
 
-async def getRandomTrack():
+async def playRandomTrack():
     async with app:
+        global start
+
+        print(time.time() - start)
         row = trackDB[random.randrange(len(trackDB))]
         tracksStr = row[0] + " " + row[1]
         # print(tracksStr)
+
+        start = time.time()
         bot_results = await app.get_inline_bot_results(
             "murglar_bot",
             query=tracksStr
@@ -68,28 +68,25 @@ async def getRandomTrack():
         await app.send_inline_bot_result(
             "me", bot_results.query_id,
             bot_results.results[0].id)
+        print(time.time() - start)
 
-        # await asyncio.sleep(8)
         trackMessage = await get_last_message()
         while trackMessage.reply_markup is not None:
             await asyncio.sleep(1)
             trackMessage = await get_last_message()
 
-        # print(trackMessage)
-        # await app.edit_message_media(
-        #     chat_id=trackMessage.chat.id,
-        #     message_id=trackMessage.id,
-        #     media=InputMediaAudio(
-        #         caption="lol.mp3",
-        #         media=str(trackMessage.audio.file_id)))
-        await app.send_audio(
-            chat_id="me",
-            audio=str(trackMessage.audio.file_id),
-            title="Title", performer="Performer",
-        )
         trackMessage = await get_last_message()
-        # print(trackMessage)
-        await downloadTrackFromMessage(trackMessage)
+        print(trackMessage)
+
+        downloadTrackTask = asyncio.create_task(downloadTrackFromMessage(trackMessage))
+        trackFile = await downloadTrackTask
+        await app.delete_messages("me", trackMessage.id)
+        playSongFromFile(trackFile)
+
+
+async def appMain():
+    while True:
+        await playRandomTrack()
 
 
 async def progress(current, total):
@@ -101,13 +98,13 @@ async def my_handler(client, message):
     await downloadTrackFromMessage(message)
 
 
-async def main():
+async def main228():
     async with app:
         await app.run()
 
-
-app.run(getRandomTrack())
-# asyncio.run(main())
+start = time.time()
+app.run(appMain())
+# asyncio.run(main228())
 
 
 # contact_phone = '+79538571643'
