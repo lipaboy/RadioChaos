a5f0c57379557b638dff7d7aff71bb201cc5d43e
a.lipatkin <a.lipatkin@vega-absolute.ru>
2023-09-05 14:49:14 +0700

    Добавил новые штучки

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ with open("tracksless.txt", encoding='utf-8') as tracksFile:
         songName = tracksFile.readline().strip()
         if len(songName) <= 0:
             break
-        trackRow = (groupName, songName)
+        dateStr = tracksFile.readline().strip()
+        if len(dateStr) <= 0:
+            break
+        trackRow = (groupName, songName, dateStr)
         trackDB.append(trackRow)
 
 
@@ -42,67 +45,88 @@ async def downloadTrackFromMessage(message):
     return file
 
 
-def playSongFromFile(file):
-    mixer.init()
-    mixer.music.load(io.BytesIO(file.getbuffer()))
-    mixer.music.play()
+def playSongFromFile(filename, startPlaying: float = 0.0):
+    mixer.music.load(filename)
+    mixer.music.play(start=startPlaying, fade_ms=1000)
+    print(f"start second: {startPlaying}")
     while mixer.music.get_busy():  # wait for music to finish playing
         time.sleep(1)
 
 
-async def playRandomTrack():
+async def playRandomTrack(fromBeginning: bool):
     async with app:
-        global start
+        global _startMeasure
+        print(time.time() - _startMeasure)
 
-        print(time.time() - start)
-        row = trackDB[random.randrange(len(trackDB))]
-        tracksStr = row[0] + " " + row[1]
-        # print(tracksStr)
+        while True:
+            row = trackDB[random.randrange(len(trackDB))]
+            tracksStr = row[0] + " " + row[1]
+            # print(tracksStr)
 
-        start = time.time()
-        bot_results = await app.get_inline_bot_results(
-            "murglar_bot",
-            query=tracksStr
-        )
+            _startMeasure = time.time()
+            bot_results = await app.get_inline_bot_results(
+                "murglar_bot",
+                query=tracksStr
+            )
+
+            if bot_results.results[0].content.attributes[0].duration >= 25:
+                break
 
         await app.send_inline_bot_result(
             "me", bot_results.query_id,
             bot_results.results[0].id)
-        print(time.time() - start)
+        print(time.time() - _startMeasure)
 
+        # wait when track was loaded to telegram's server
+
+        _startMeasure = time.time()
         trackMessage = await get_last_message()
+        # print(trackMessage)
         while trackMessage.reply_markup is not None:
             await asyncio.sleep(1)
             trackMessage = await get_last_message()
-
         trackMessage = await get_last_message()
-        print(trackMessage)
+        print(time.time() - _startMeasure)
+        # print(trackMessage.audio)
 
         downloadTrackTask = asyncio.create_task(downloadTrackFromMessage(trackMessage))
         trackFile = await downloadTrackTask
         await app.delete_messages("me", trackMessage.id)
-        playSongFromFile(trackFile)
+
+        # playSongFromFile("radioInit.mp3", 6)
+        # playSongFromFile(
+        #     io.BytesIO(trackFile.getbuffer()),
+        #     0 if fromBeginning else random.randrange(0, trackMessage.audio.duration - 20))
+
+        radioInitSound.play(maxtime=6000)
+        await asyncio.sleep(5)
+        mixer.music.load(io.BytesIO(trackFile.getbuffer()))
+        # mixer.mu
+        mixer.music.play(start=0)
+        while mixer.music.get_busy():  # wait for music to finish playing
+            await asyncio.sleep(1)
+        # mixer.music.load(io.BytesIO(trackFile.getbuffer()))
+        # mixer.music.play(start=0)
+        # while mixer.music.get_busy():  # wait for music to finish playing
+        #     await asyncio.sleep(1)
+
+        _startMeasure = time.time()
 
 
 async def appMain():
+    await playRandomTrack(False)
     while True:
-        await playRandomTrack()
+        await playRandomTrack(True)
 
 
 async def progress(current, total):
     print(f"{current * 100 / total:.1f}%")
 
 
-@app.on_message(filters.me)
-async def my_handler(client, message):
-    await downloadTrackFromMessage(message)
+mixer.init()
+radioInitSound = mixer.Sound("radioInit.ogg")
 
-
-async def main228():
-    async with app:
-        await app.run()
-
-start = time.time()
+_startMeasure = time.time()
 app.run(appMain())
 # asyncio.run(main228())
 
--- reformatTracksDB.py
+++ reformatTracksDB.py
@@ -1,6 +1,5 @@
 import csv
 
-
 with open("tracks.csv", encoding='utf-8') as fp:
     reader = csv.reader(fp, delimiter=",", quotechar='"')
     # next(reader, None)  # skip the headers
@@ -8,12 +7,26 @@ with open("tracks.csv", encoding='utf-8') as fp:
 
 tracksDB.pop(0)
 
+attrs = ["artist", "artist_spotify_id", "name", "spotify_id",
+         "duration_ms", "explicit", "popularity",
+         "album_type", "album_name", 'album_spotify_id',
+         'release_date', 'album_popularity', 'key', 'mode',
+         'time_signature', 'acousticness',
+         'danceability', 'energy', 'instrumentalness',
+         'liveness', 'loudness', 'speechiness', 'valence', 'tempo']
+
 with open('tracksless.txt', mode="w", encoding='utf-8') as outFile:
     for row in tracksDB:
-        groupName = row[0].strip()
-        songName = row[2].strip()
+        groupName = row[attrs.index('artist')].strip()
+        songName = row[attrs.index('name')].strip()
+        dateStr = row[attrs.index('release_date')].strip()
+        dateStr = dateStr[:4]
+        if not dateStr.isdigit():
+            dateStr = "None"
         if len(groupName) > 0 and len(songName) > 0:
-            lessData = groupName + "\n" + songName + "\n"
+            lessData = groupName + "\n" \
+                       + songName + "\n" \
+                       + dateStr + "\n"
             outFile.write(lessData)
         else:
             print(groupName + " : " + songName)
